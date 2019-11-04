c105cb158abeecf7a4fc3c1d8ca3a5227114fc92
hgarciaalberto <hgarcia.alberto@gmail.com>
2019-11-04 02:09:39 +0000

    Delete login unused classes

--- app/src/main/java/com/ahgitdevelopment/goosesquizzes/di/component/ViewModelFactoryModule.kt
+++ app/src/main/java/com/ahgitdevelopment/goosesquizzes/di/component/ViewModelFactoryModule.kt
@@ -1,10 +1,7 @@
 package com.ahgitdevelopment.goosesquizzes.di.component
 
 import com.ahgitdevelopment.goosesquizzes.firebase.auth.FirebaseAuthRespository
-import com.ahgitdevelopment.goosesquizzes.ui.login.LoginDataSource
-import com.ahgitdevelopment.goosesquizzes.ui.login.LoginRepository
 import com.ahgitdevelopment.goosesquizzes.viewmodel.LoginFirebaseViewModel
-import com.ahgitdevelopment.goosesquizzes.viewmodel.LoginViewModel
 import com.ahgitdevelopment.goosesquizzes.viewmodel.ViewModelFactory
 import dagger.Module
 import dagger.Provides
@@ -13,19 +10,8 @@ import javax.inject.Provider
 @Module
 class ViewModelFactoryModule {
     @Provides
-    fun viewModelFactory(
-        loginViewModelProvider: Provider<LoginViewModel>,
-        loginFirebaseViewModelProvider: Provider<LoginFirebaseViewModel>
-    ): ViewModelFactory {
-        return ViewModelFactory(
-            loginViewModelProvider,
-            loginFirebaseViewModelProvider
-        )
-    }
-
-    @Provides
-    fun getloginViewModel(): LoginViewModel {
-        return LoginViewModel(loginRepository = LoginRepository(dataSource = LoginDataSource()))
+    fun viewModelFactory(loginFirebaseViewModelProvider: Provider<LoginFirebaseViewModel>): ViewModelFactory {
+        return ViewModelFactory(loginFirebaseViewModelProvider)
     }
 
     @Provides
--- app/src/main/java/com/ahgitdevelopment/goosesquizzes/viewmodel/ViewModelFactory.kt
+++ app/src/main/java/com/ahgitdevelopment/goosesquizzes/viewmodel/ViewModelFactory.kt
@@ -9,18 +9,14 @@ import javax.inject.Provider
  * ViewModel provider factory to instantiate LoginViewModel.
  * Required given LoginViewModel has a non-empty constructor
  */
-class ViewModelFactory @Inject constructor(
-    loginViewModelProvider: Provider<LoginViewModel>,
-    loginFirebaseViewModelProvider: Provider<LoginFirebaseViewModel>
-) : ViewModelProvider.Factory {
+class ViewModelFactory @Inject constructor(loginFirebaseViewModelProvider: Provider<LoginFirebaseViewModel>) :
+    ViewModelProvider.Factory {
 
-    private var mLoginViewModelProvider: Provider<LoginViewModel> = loginViewModelProvider
     private var mLoginFirebaseViewModelProvider: Provider<LoginFirebaseViewModel> = loginFirebaseViewModelProvider
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return when (modelClass) {
-            LoginViewModel::class.java -> mLoginViewModelProvider.get() as T
             LoginFirebaseViewModel::class.java -> mLoginFirebaseViewModelProvider.get() as T
 
             else -> throw IllegalArgumentException("Unknown ViewModel class")
