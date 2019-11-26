240728138c709aadfcd3e099b70b55c9baacc091
hgarciaalberto <hgarcia.alberto@gmail.com>
2019-11-26 20:05:57 +0000

    Creation of sharedTest folder

---
+++ app/src/sharedTest/java/com/ahgitdevelopment/goosesquizzes/commontest/Constant.kt
@@ -0,0 +1,13 @@
+package com.ahgitdevelopment.goosesquizzes.commontest
+
+// region Login
+
+const val ERROR_USER: String = "admin@"
+const val VALID_USER: String = "admin"
+const val VALID_USER_MAIL: String = "admin@admin.com"
+const val VALID_PASSWORD: String = "adminadmin"
+const val WRONG_PASSWORD: String = "aaaaaaa"
+const val EMPTY_STRING: String = ""
+const val SORT_PASSWORD: String = "12"
+
+// endregion
